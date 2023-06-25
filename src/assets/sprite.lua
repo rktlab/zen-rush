f90ab9399a09f815eade5ad289c517a0d5fb12c9
Mayeu <m@mayeu.me>
2023-06-25 13:15:04 +0200

    feat: update the map

---
+++ src/assets/sprite.lua
@@ -0,0 +1,452 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.1",
+  name = "sprite",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 10,
+  image = "sprite.png",
+  imagewidth = 160,
+  imageheight = 112,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 70,
+  tiles = {
+    {
+      id = 0,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 1,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 2,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 3,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 4,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 5,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 6,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 7,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 8,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 9,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 10,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 11,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 12,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 13,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 14,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 15,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 16,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 17,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 18,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 19,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 20,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 21,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 22,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 23,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 24,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 25,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 26,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 27,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 28,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 29,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 30,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 31,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 32,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 33,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 34,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 35,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 36,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 37,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 38,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 39,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 40,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 41,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 42,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 43,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 44,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 45,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 46,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 47,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 48,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 49,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 50,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 51,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 52,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 53,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 54,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 55,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 56,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 57,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 58,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 59,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 60,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 61,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 62,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 63,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 64,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 65,
+      properties = {
+        ["colision"] = false
+      }
+    },
+    {
+      id = 66,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 67,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 68,
+      properties = {
+        ["colision"] = true
+      }
+    },
+    {
+      id = 69,
+      properties = {
+        ["colision"] = true
+      }
+    }
+  }
+}
